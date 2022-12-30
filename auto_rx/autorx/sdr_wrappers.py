0a96803f0514adb8cfb7184a6e05ba5a1d99545f
AstroTaka <astrotaka@mtd.homeip.net>
2022-12-31 01:05:51 +0900

    Enable TunerAGC and RTL AGC when gain=-2 at RTL-SDR

--- auto_rx/autorx/sdr_wrappers.py
+++ auto_rx/autorx/sdr_wrappers.py
@@ -192,7 +192,6 @@ def get_sdr_iq_cmd(
     dc_block: bool = False,
     ppm = 0,
     gain = None,
-    agc  = None,
     bias = False,
     sdr_hostname = "",
     sdr_port = 5555,
@@ -241,11 +240,6 @@ def get_sdr_iq_cmd(
             if gain >= 0:
                 _gain = f"-g {gain:.1f} "
             elif gain == -2:
-                _agc = f"-E agc "
-
-        _agc = ""
-        if agc:
-            if agc >= 1:
                 _agc = f"-E agc "
 
         _cmd = (
@@ -329,13 +323,11 @@ def get_sdr_fm_cmd(
 
     if sdr_type == "RTLSDR":
         _gain = ""
+        _agc = ""
         if gain:
             if gain >= 0:
                 _gain = f"-g {gain:.1f} "
-
-        _agc = ""
-        if agc:
-            if agc >= 1:
+            elif gain == -2:
                 _agc = f"-E agc "
 
         _cmd = (
