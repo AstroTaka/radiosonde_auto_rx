27faed6901066d34e2b48bd3844b14f68628af2c
AstroTaka <astrotaka@mtd.homeip.net>
2023-01-10 12:22:59 +0900

    fix wrong merge

--- auto_rx/autorx/log_files.py
+++ auto_rx/autorx/log_files.py
@@ -68,9 +68,6 @@ def log_filename_to_stats(filename, quicklook=False):
         # Third field is the sonde type, in 'shortform'
         _type = _fields[2]
 
-        if _type=="MEISEI":
-            _type = _fields[1].split("-")[0]
-
         _type_str = short_type_lookup(_type)
         _short_type = short_short_type_lookup(_type)
 
